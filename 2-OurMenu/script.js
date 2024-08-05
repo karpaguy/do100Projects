14d587e9c7da5ff6c910431811dc71d6cac6a390
karpaguy <denuzzidouglas@gmail.com>
2024-08-05 17:10:40 -0300

    Populate html; credits in txt for now

---
+++ 2-OurMenu/script.js
@@ -0,0 +1,41 @@
+const menu = [
+    {
+        id: 1,
+        title: "Panquecas com Frutas",
+        category: "cafe-manha",
+        price: 15.99,
+        img: "./images/Pancakes_with_fruit.jpg",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat error facilis odit corrupti modi sunt ipsam itaque, incidunt quo aspernatur?"
+    },
+    {
+        id: 2,
+        title: "Bolo Sabooroso",
+        category: "cafe-manha",
+        price: 15.99,
+        img: "./images/Pancakes_with_fruit.jpg",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat error facilis odit corrupti modi sunt ipsam itaque, incidunt quo aspernatur?"
+    }
+];
+
+const sectionCenter = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', function() {
+    let displayMenu = menu.map(function(item) {
+        // console.log(item)
+        return `<article class="menu-item">
+            <img src=${item.img} class="photo" alt=${item.title}>
+            <div class="item-info">
+                <header>
+                    <h4>${item.title}</h4>
+                    <h4 class="price">$${item.price}</h4>
+                </header>
+                <p class="item-text">
+                ${item.desc}
+                </p>
+            </div>
+        </article>`;
+    });
+
+    displayMenu = displayMenu.join(''); // Impede de aparecer uma vírgula entre elementos, que pode atrapalhar.
+    sectionCenter.innerHTML = displayMenu;
+})
