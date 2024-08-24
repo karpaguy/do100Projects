4bd79454ca863f50bb2250c410942a6db1a35393
karpaguy <denuzzidouglas@gmail.com>
2024-08-23 22:21:59 -0300

    Game script working, CSS need more working

--- 5-Fight/README.md
+++ 5-Fight/README.md
@@ -1,2 +1,8 @@
 The ideia is to make it work, then optimize it.
 Also, the enemies must have its turn as a function, in case I want to add more things in its turn.
+
+Attributions for now...
+-
+Attribution Instructions: 
+Attribution is not required; if you want, then attribute it to blarumyrran@gmail.com .
+-
--- 5-Fight/script.js
+++ 5-Fight/script.js
@@ -1,1 +1,45 @@
-console.log('hi')
+const attackBtn = document.querySelector('#attack');
+const shieldBtn = document.querySelector('#shield');
+
+const enemy = {
+    Life: 100,
+    Damage: 20,
+    Attack_time: 2,
+};
+const player = {
+    Life: 30,
+    Damage: 20,
+    Shield: false,
+};
+
+let roundTimer = 0;
+
+function turnPlayer(action) {
+    // The PLAYER will no longer be protect at start of HIS turn.
+    player["Shield"] = false;
+    switch(action) {
+        case "Attack": 
+            enemy["Life"] -= player["Damage"];
+            break;
+        case "Shield": 
+            player["Shield"] = true;
+            break;
+    }
+}
+function turnEnemy() {
+    // Turn this into a ternary operator later.
+    if (player["Shield"] === false && (roundTimer % enemy["Attack_time"]) == 0 ) 
+        {
+            player["Life"] -= enemy["Damage"]
+        }
+};
+function combatRound(action){
+    roundTimer++
+    console.log(`Turn === ${roundTimer}`);
+    turnPlayer(action);
+    turnEnemy();
+    console.log(`Player Life = ${player.Life} || Enemy Life = ${enemy.Life}`);
+};
+
+attackBtn.addEventListener('click', () => combatRound("Attack"))
+shieldBtn.addEventListener('click', () => combatRound("Shield"))
