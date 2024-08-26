61002d70368217e52bf9ccc3940c141f7dba15c1
karpaguy <denuzzidouglas@gmail.com>
2024-08-25 22:55:24 -0300

    More CSS done, and elements added for script

--- 5-Fight/script.js
+++ 5-Fight/script.js
@@ -1,45 +1,44 @@
-const attackBtn = document.querySelector('#attack');
-const shieldBtn = document.querySelector('#shield');
-
-const enemy = {
-    Life: 100,
-    Damage: 20,
-    Attack_time: 2,
-};
-const player = {
-    Life: 30,
-    Damage: 20,
-    Shield: false,
-};
-
-let roundTimer = 0;
-
-function turnPlayer(action) {
-    // The PLAYER will no longer be protect at start of HIS turn.
-    player["Shield"] = false;
-    switch(action) {
-        case "Attack": 
-            enemy["Life"] -= player["Damage"];
-            break;
-        case "Shield": 
-            player["Shield"] = true;
-            break;
+const wordList = [
+    {
+        id: 1,
+        word: "flask",
+        similiar: ["bottle", "cup", "kettle", "bucket", "mug", "glass"]
+    },
+    {
+        id: 2,
+        word: "finger",
+        similiar: ["hand"]
     }
-}
-function turnEnemy() {
-    // Turn this into a ternary operator later.
-    if (player["Shield"] === false && (roundTimer % enemy["Attack_time"]) == 0 ) 
-        {
-            player["Life"] -= enemy["Damage"]
-        }
-};
-function combatRound(action){
-    roundTimer++
-    console.log(`Turn === ${roundTimer}`);
-    turnPlayer(action);
-    turnEnemy();
-    console.log(`Player Life = ${player.Life} || Enemy Life = ${enemy.Life}`);
-};
+]
 
-attackBtn.addEventListener('click', () => combatRound("Attack"))
-shieldBtn.addEventListener('click', () => combatRound("Shield"))
+
+// const palavras = [
+//     {
+//         id: 1
+//         palavra: "não"
+//         semissinônimo: ["negativo", "errado"],
+//     },
+//     {
+//         id: 2
+//         palavra: "garrafa"
+//         semissinônimo: ["frasco", "jarra"],
+//     }
+// ]
+
+// let score = 0;
+// const lacuna = queryDocument('.lacuna');
+
+// function rodada() {
+//     let number = random();
+//     lacuna.propriedadeTexto = palavras[number].palavra
+
+//     if (palavraJogador == conferirResposta(number)) {
+//         score++
+//     } else {
+//         score--
+//     }
+// };
+
+// function conferirResposta(n) {
+//     procurar(palavraJogador) em palavras[n].semissinônimo;
+// };
